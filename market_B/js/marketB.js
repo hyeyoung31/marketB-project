b00b1156fd41f5c53a8a40205de353f06f5cebde
hyeyoung31 <154103645+hyeyoung31@users.noreply.github.com>
2024-02-14 21:09:03 +0900

    first commit
    ·마켓비 PC 메인페이지 퍼블리싱
    ·배너(swiper.js), 아이콘 메뉴, 탭(javascript), 푸터
    
    2/14일 모임 결과
    · 피그마 (메인페이지, 상세페이지 구성) 점검
    · PC 메인페이지 퍼블리싱 점검 
    · 디자인 배너 점검
    
    ·역할 분배
      우찬: 어드민 관리자 대쉬보드 수정, 메인페이지 피그마 레이어 정리
      수아: 상품 목록상세, 상세페이지 피그마 1차 수정
      민아: 상품 박스 레이아웃 디자인, 매거진B 영역에 들어가는 사진 컨셉 디자인
      혜영: 메인 탭메뉴 자바스크립트 완성. 상세페이지 코딩 레이아웃 들어가기

---
+++ market_B/js/marketB.js
@@ -0,0 +1,29 @@
+
+// 주메뉴 탭( BEST ITEM, NEW ARRIVAL, MD'S CHOICE) 첫번째 방법
+var targetLink = document.querySelectorAll('.tab_menu a');
+var tabContent = document.querySelectorAll('.panel >li ');
+
+
+for(var i=0; i<targetLink.length; i++){
+
+targetLink[i].addEventListener('click', function(e){
+   e.preventDefault();
+   var orgTarget = e.target.getAttribute('href');
+   console.log(orgTarget);
+   var tabTarget = orgTarget.replace('#','');
+
+   for(var x =0; x < tabContent.length; x++){
+      tabContent[x].style.display ='none'
+   }
+ document.getElementById(tabTarget).style.display = 'block';
+
+ for(var j = 0; j<targetLink.length; j++){
+       targetLink[j].classList.remove('selected');
+       e.target.classList.add('selected');
+ }
+});   
+}
+
+document.getElementById('tab1').style.display = 'block';
+
+ 
