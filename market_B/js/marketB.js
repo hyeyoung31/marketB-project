73eec433e59022b29342cba61877ad637e155aaa
hyeyoung31 <154103645+hyeyoung31@users.noreply.github.com>
2024-02-29 23:25:20 +0900

    third commit
    -이중탭(nested tab) jquery로 수정

--- market_B/js/marketB.js
+++ market_B/js/marketB.js
@@ -1,60 +1,38 @@
-var nestedTabSelect = (tabsElement, currentElement) => {
-   const tabs = tabsElement ?? 'ul.tabs';
+$(function () {
 
-   const currentClass = currentElement ?? 'active';
-   console.log(currentClass);
+  const tabLink = $('.tabs li a');
+  const nestedLink = $('.nested_tabs li a'); 
 
-   var test =  document.querySelectorAll(tabs)
-   console.log(test);
-   document.querySelectorAll(tabs).forEach(function (tabContainer) {
-     let activeLink, activeContent;
-     const links = Array.from(tabContainer.querySelectorAll("a"));
-     console.log(links);
+  tabLink.on("click", function (e) {
+    e.preventDefault();
+    const target = $(this).attr('href');
 
-     
+    tabLink.removeClass('active');
+    $(this).addClass('active');
+    $(target).addClass('active').siblings('.active').removeClass('active');
 
+    const nestedLinkActive = $(target).find('.nested_tabs li a.active');
 
-     activeLink = links.find(function (link) {
-         return link.getAttribute("href") === location.hash;
-       }) || links[0];
-       console.log(links[0]);
-     console.log(activeLink);
-     activeLink.classList.add(currentClass);
-   
- 
-     activeContent = document.querySelector(activeLink.getAttribute("href"));
-      console.log(activeContent);
-     activeContent.classList.add(currentClass);
- 
-     links.forEach(function (link) {
-       if (link !== activeLink) {
-         const content = document.querySelector(link.getAttribute("href"));
-         // console.log(content);
-         content.classList.remove(currentClass);
-       }
-     });
- 
-     tabContainer.addEventListener("click", function (e) {
-       if (e.target.tagName === "A") {
+    if (!nestedLinkActive.length) {
+      $(target).find('.nested_tabs li:first-child a').trigger('click');
+    } else {
+      nestedLinkActive.trigger('click');
+    }
+  });
 
-         activeLink.classList.remove(currentClass);
-         activeContent.classList.remove(currentClass);
- 
+  nestedLink.on("click", function (e) {
+    e.preventDefault();
+    const target = $(this).attr("href");
+    nestedLink.removeClass('active');
+    $(this).addClass('active');
+    $(target).addClass('active').siblings('.active').removeClass("active");
+  });
 
-         activeLink = e.target;
-         console.log(activeLink);
-         
-         activeContent = document.querySelector(activeLink.getAttribute("href"));
-         console.log(activeContent);
-         activeLink.classList.add(currentClass);
-         activeContent.classList.add(currentClass);
- 
-         e.preventDefault();
-       }
-     });
-   });
- };
- 
- nestedTabSelect('ul.tabs', 'active');
+  const tabLinkActive = tabLink.filter('.active');
+  if (!tabLinkActive.length) {
+    tabLink.first().trigger('click');
+  } else {
+    tabLinkActive.trigger('click');
+  }
 
- 
+});
